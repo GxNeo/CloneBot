e6face85ac6099109c2e9ea2d9d7a1000e566b18
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-05-01 14:13:23 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+import os
+from pyrogram import Client
+from config import Config
+from config import LOGGER
+
+class User(Client):
+    def __init__(self):
+        super().__init__(
+            session_name=Config.TG_USER_SESSION,
+            api_id=Config.API_ID,
+            api_hash=Config.API_HASH,           
+        )
+        self.LOGGER = LOGGER
+
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        self.LOGGER(__name__).info(
+            f"@{me.username} started!"
+        )
+        return self, me.id
+
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER(__name__).info("Bot stopped. Bye.")
