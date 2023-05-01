6177d207f515d922d2092cc842054e2970b80a96
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-05-01 14:16:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from config import LOGGER
 class User(Client):
     def __init__(self):
         super().__init__(
-            session_name=Config.TG_USER_SESSION,
+            session_string=Config.TG_USER_SESSION,
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,           
         )
